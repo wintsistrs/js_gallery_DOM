918645eb3299b10e022eaf9fa7499d8e8f2767bd
Anastasiia Paliha <nastya.paliha@gmail.com>
2024-12-14 22:17:59 +0200

    make build

---
+++ index.0d3982d9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link"),l=e.target.closest(".gallery__thumb");if(e.preventDefault(),r||l){var a=r.getAttribute("href");t.setAttribute("src",a)}});
+//# sourceMappingURL=index.0d3982d9.js.map
